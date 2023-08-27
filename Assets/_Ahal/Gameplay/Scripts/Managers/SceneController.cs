10e93b6b089b09b341bf8db53bd83b2077e10587
Seanushka <Sean.nechaev@gmail.com>
2023-08-27 23:03:23 +0300

    Working Menu !! yeepy

--- Assets/_Ahal/Gameplay/Scripts/Managers/SceneController.cs
+++ Assets/_Ahal/Gameplay/Scripts/Managers/SceneController.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class SceneController : MonoBehaviour
 {
+
     public void MoveToNextScene()
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
